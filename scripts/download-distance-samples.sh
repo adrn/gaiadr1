69b1ff1f8818c4dfd4e25d8d5b9b18a4597a0bd5
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-16 20:19:08 -0400

    dots

--- scripts/download-distance-samples.sh
+++ scripts/download-distance-samples.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-wget http://s3.adrian.pw/distance-samples-01.hdf5
-wget http://s3.adrian.pw/distance-samples-02.hdf5
-wget http://s3.adrian.pw/distance-samples-03.hdf5
-wget http://s3.adrian.pw/distance-samples-04.hdf5
-wget http://s3.adrian.pw/distance-samples-05.hdf5
-wget http://s3.adrian.pw/distance-samples-06.hdf5
-wget http://s3.adrian.pw/distance-samples-07.hdf5
-wget http://s3.adrian.pw/distance-samples-08.hdf5
-wget http://s3.adrian.pw/distance-samples-09.hdf5
-wget http://s3.adrian.pw/distance-samples-10.hdf5
-wget http://s3.adrian.pw/distance-samples-11.hdf5
-wget http://s3.adrian.pw/distance-samples-12.hdf5
-wget http://s3.adrian.pw/distance-samples-13.hdf5
-wget http://s3.adrian.pw/distance-samples-14.hdf5
-wget http://s3.adrian.pw/distance-samples-15.hdf5
-wget http://s3.adrian.pw/distance-samples-16.hdf5
+wget http://s3.adrian.pw/distance-samples-01.hdf5 .
+wget http://s3.adrian.pw/distance-samples-02.hdf5 .
+wget http://s3.adrian.pw/distance-samples-03.hdf5 .
+wget http://s3.adrian.pw/distance-samples-04.hdf5 .
+wget http://s3.adrian.pw/distance-samples-05.hdf5 .
+wget http://s3.adrian.pw/distance-samples-06.hdf5 .
+wget http://s3.adrian.pw/distance-samples-07.hdf5 .
+wget http://s3.adrian.pw/distance-samples-08.hdf5 .
+wget http://s3.adrian.pw/distance-samples-09.hdf5 .
+wget http://s3.adrian.pw/distance-samples-10.hdf5 .
+wget http://s3.adrian.pw/distance-samples-11.hdf5 .
+wget http://s3.adrian.pw/distance-samples-12.hdf5 .
+wget http://s3.adrian.pw/distance-samples-13.hdf5 .
+wget http://s3.adrian.pw/distance-samples-14.hdf5 .
+wget http://s3.adrian.pw/distance-samples-15.hdf5 .
+wget http://s3.adrian.pw/distance-samples-16.hdf5 .
